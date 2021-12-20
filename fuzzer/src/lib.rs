02f30ca1f833815e435544cf21d17342d0581c07
docfate111 <tdwilliamsiv@gmail.com>
2021-12-20 14:19:29 -0500

    btrfs parser as lib

--- fuzzer/src/lib.rs
+++ fuzzer/src/lib.rs
@@ -1,8 +1,8 @@
-pub const BTRFS_SUPERBLOCK_MAGIC: [u8; 8] = *b"_BHRfS_M";
-
 mod btrfs_parse {
     use memmap::{Mmap, MmapOptions};
     use std::fs::File;
+    use std::io::Write;
+    pub const BTRFS_SUPERBLOCK_MAGIC: [u8; 8] = *b"_BHRfS_M";
     fn map_to_file(filename: &str) -> Result<Mmap, &'static str> {  
         let file = match File::open(filename) {
             Err(_) => { 
@@ -20,16 +20,40 @@ mod btrfs_parse {
         };
     }
 
-    /*fn parse_block(memmapd: &Mmap) -> Vec<&[u8]> {
-        let magic_offsets: [u32; 3] =         
+    fn parse_block(memmapd: &Mmap, out: &mut File) -> bool {
+        let magic_offsets: [usize; 3] = [0x10_040, 0x4_000_040, 0x4_000_000_040];
+        let mut parsed = false;
+        for offset in magic_offsets {
+            match memmapd.get(offset..offset+8) {
+                Some(v) => {
+                    if v == BTRFS_SUPERBLOCK_MAGIC {
+                        println!("found magic btrfs header at {:#01x}", offset);
+                        let start = offset - 0x40;
+                        match memmapd.get(start .. start + 0xdcb) {
+                            Some(block) => {
+                                parsed = true;
+                                out.write_all(block);
+                            },
+                            None => {
+                                continue;
+                            },
+                        }
+                    }
+                },
+                None => {
+                    continue;
+                }
+            }
+        }
+        parsed
     }
 
-    pub fn extract(input_name: &'a str, output_name: &'a str) -> Result<(), Error> {
+    /*pub fn extract(input_name: &'a str, out_name: &'a str) -> Result<(), Error> {
     
         Ok(())
-    }
+    }*/
 
-    pub fn insert(metadata: &str, file_to_insert: &str) -> {
+    /*pub fn insert(metadata: &str, file_to_insert: &str) -> {
 
     }*/
 }
